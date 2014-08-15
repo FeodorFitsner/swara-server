4e26b2b68985acfdbd1f129ead9c20644b1a1325
Floyd Pink <imbleedingme@googlemail.com>
2014-08-15 13:42:45 -0400

    change logo, favicon and the homepage

--- config/express.js
+++ config/express.js
@@ -33,7 +33,7 @@ module.exports = function(db) {
 	app.locals.title = config.app.title;
 	app.locals.description = config.app.description;
 	app.locals.keywords = config.app.keywords;
-	// app.locals.facebookAppId = config.facebook.clientID;
+	app.locals.facebookAppId = '1473022056285622';  // hard-coded with the Swara-Server facebook app Id
 	app.locals.jsFiles = config.getJavaScriptAssets();
 	app.locals.cssFiles = config.getCSSAssets();
 
